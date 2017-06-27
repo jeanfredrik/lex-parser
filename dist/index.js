a7c21663158e02efeb3994c95a9231f3e82507a5
jeanfredrik <jeanfredrik@gmail.com>
2017-06-27 21:24:20 +0200

    🐛 Fix exports

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,15 @@
 var grammar = require("./lex.js");
 var { Parser } = require("nearley");
 
-export function parser() {
+function parser() {
   return new Parser(grammar.ParserRules, grammar.ParserStart);
 }
 
-export default parser;
-
-export function parse(input) {
+function parse(input) {
   const p = parser();
   p.feed(input);
   return p.results;
 }
+
+module.exports = parser;
+module.exports.parse = parse;
