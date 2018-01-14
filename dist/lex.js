c4e3cbdfd1d64a35dfd614d6ed6ceb85fc6f2640
Fredrik Johansson <jeanfredrik@gmail.com>
2018-01-14 10:24:14 +0100

    📦 Build grammar and docs

--- dist/lex.js
+++ dist/lex.js
@@ -19,7 +19,16 @@ var grammar = {
         })
           },
     {"name": "Statement", "symbols": ["Definition"], "postprocess": id},
-    {"name": "Statement", "symbols": ["Pattern"], "postprocess": id},
+    {"name": "Statement", "symbols": ["PatternWithWeight"], "postprocess": id},
+    {"name": "Statement", "symbols": ["Comment"], "postprocess": id},
+    {"name": "Comment$ebnf$1", "symbols": []},
+    {"name": "Comment$ebnf$1", "symbols": ["Comment$ebnf$1", /[^\n\r]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Comment", "symbols": [{"literal":"#"}, "Comment$ebnf$1"], "postprocess": 
+        ([, comment]) => ({
+          type: 'Comment',
+          comment: comment.join(''),
+        })
+          },
     {"name": "Definition", "symbols": ["Identifier", "_", {"literal":"="}, "DefList"], "postprocess": 
         ([identifier, , , items]) => ({
           type: 'Definition',
@@ -37,11 +46,73 @@ var grammar = {
     {"name": "DefListItem$subexpression$1", "symbols": ["_", "Grapheme", "_"]},
     {"name": "DefListItem", "symbols": ["DefListItem$subexpression$1"], "postprocess": d => d[0][1]},
     {"name": "DefListItem", "symbols": ["_"], "postprocess": d => ''},
-    {"name": "Pattern", "symbols": ["Grapheme"], "postprocess": 
-        d => ({
-          type: 'Pattern',
-          pattern: d[0],
+    {"name": "PatternWithWeight", "symbols": ["Pattern"], "postprocess": 
+        ([pattern]) => Object.assign({weight: 1}, pattern)
+          },
+    {"name": "PatternWithWeight", "symbols": ["Pattern", "_", "PatternWeight"], "postprocess": 
+        ([pattern, , weight]) => Object.assign({}, pattern, {
+          weight,
         })
+          },
+    {"name": "PatternFragment", "symbols": ["Grapheme"], "postprocess": 
+        ([grapheme]) => ({
+          type: 'PatternFragment',
+          grapheme,
+        })
+          },
+    {"name": "Pattern$ebnf$1", "symbols": []},
+    {"name": "Pattern$ebnf$1$subexpression$1$ebnf$1", "symbols": ["SubPattern"]},
+    {"name": "Pattern$ebnf$1$subexpression$1$ebnf$1", "symbols": ["Pattern$ebnf$1$subexpression$1$ebnf$1", "SubPattern"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Pattern$ebnf$1$subexpression$1$ebnf$2", "symbols": ["PatternFragment"], "postprocess": id},
+    {"name": "Pattern$ebnf$1$subexpression$1$ebnf$2", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "Pattern$ebnf$1$subexpression$1", "symbols": ["Pattern$ebnf$1$subexpression$1$ebnf$1", "Pattern$ebnf$1$subexpression$1$ebnf$2"]},
+    {"name": "Pattern$ebnf$1", "symbols": ["Pattern$ebnf$1", "Pattern$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Pattern", "symbols": ["PatternFragment", "Pattern$ebnf$1"], "postprocess": 
+        ([fragment, rest]) => ({
+          type: 'Pattern',
+          parts: [
+            fragment,
+            ...rest.map(([subPatterns, fragment]) => [
+              ...subPatterns,
+              ...(fragment == null ? [] : [fragment])
+            ]).reduce((a, b) => a.concat(b), [])
+          ]
+        })
+          },
+    {"name": "Pattern$ebnf$2$subexpression$1$ebnf$1", "symbols": ["SubPattern"]},
+    {"name": "Pattern$ebnf$2$subexpression$1$ebnf$1", "symbols": ["Pattern$ebnf$2$subexpression$1$ebnf$1", "SubPattern"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Pattern$ebnf$2$subexpression$1$ebnf$2", "symbols": ["PatternFragment"], "postprocess": id},
+    {"name": "Pattern$ebnf$2$subexpression$1$ebnf$2", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "Pattern$ebnf$2$subexpression$1", "symbols": ["Pattern$ebnf$2$subexpression$1$ebnf$1", "Pattern$ebnf$2$subexpression$1$ebnf$2"]},
+    {"name": "Pattern$ebnf$2", "symbols": ["Pattern$ebnf$2$subexpression$1"]},
+    {"name": "Pattern$ebnf$2$subexpression$2$ebnf$1", "symbols": ["SubPattern"]},
+    {"name": "Pattern$ebnf$2$subexpression$2$ebnf$1", "symbols": ["Pattern$ebnf$2$subexpression$2$ebnf$1", "SubPattern"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Pattern$ebnf$2$subexpression$2$ebnf$2", "symbols": ["PatternFragment"], "postprocess": id},
+    {"name": "Pattern$ebnf$2$subexpression$2$ebnf$2", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "Pattern$ebnf$2$subexpression$2", "symbols": ["Pattern$ebnf$2$subexpression$2$ebnf$1", "Pattern$ebnf$2$subexpression$2$ebnf$2"]},
+    {"name": "Pattern$ebnf$2", "symbols": ["Pattern$ebnf$2", "Pattern$ebnf$2$subexpression$2"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Pattern", "symbols": ["Pattern$ebnf$2"], "postprocess": 
+        ([rest]) => ({
+          type: 'Pattern',
+          parts: rest.map(([subPatterns, fragment]) => [
+            ...subPatterns,
+            ...(fragment == null ? [] : [fragment])
+          ]).reduce((a, b) => a.concat(b), [])
+        })
+          },
+    {"name": "SubPattern", "symbols": [{"literal":"("}, "Pattern", {"literal":")"}], "postprocess": 
+        ([, pattern, ]) => Object.assign({weight: .5}, pattern)
+          },
+    {"name": "SubPattern", "symbols": [{"literal":"("}, "Pattern", "_", "PatternWeight", {"literal":")"}], "postprocess": 
+        ([, pattern, , weight, ]) => Object.assign({}, pattern, {
+          weight,
+        })
+          },
+    {"name": "PatternWeight", "symbols": [{"literal":"*"}, "_", "Float"], "postprocess": 
+        ([, , value]) => value
+          },
+    {"name": "PatternWeight", "symbols": [{"literal":"/"}, "_", "Float"], "postprocess": 
+        ([, , value]) => 1 / value
           },
     {"name": "Identifier$subexpression$1", "symbols": ["Alpha"]},
     {"name": "Identifier$subexpression$1", "symbols": ["Unicode"]},
@@ -67,12 +138,34 @@ var grammar = {
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", {"literal":" "}], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": d => null},
+    {"name": "Space$ebnf$1", "symbols": [{"literal":" "}]},
+    {"name": "Space$ebnf$1", "symbols": ["Space$ebnf$1", {"literal":" "}], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Space", "symbols": ["Space$ebnf$1"], "postprocess": d => null},
     {"name": "Newlines$ebnf$1", "symbols": [/[\n\r]/]},
     {"name": "Newlines$ebnf$1", "symbols": ["Newlines$ebnf$1", /[\n\r]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "Newlines", "symbols": ["Newlines$ebnf$1"], "postprocess": d => null},
     {"name": "Alpha", "symbols": [/[A-Za-z]/], "postprocess": id},
     {"name": "Unicode", "symbols": [/[^\x00-\xA0]/], "postprocess": id},
-    {"name": "Digit", "symbols": [/[0-9]/], "postprocess": id}
+    {"name": "Digit", "symbols": [/[0-9]/], "postprocess": id},
+    {"name": "Integer", "symbols": [{"literal":"0"}], "postprocess": () => 0},
+    {"name": "Integer$ebnf$1", "symbols": []},
+    {"name": "Integer$ebnf$1", "symbols": ["Integer$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Integer", "symbols": [/[1-9]/, "Integer$ebnf$1"], "postprocess": ([first, rest]) => Number([first, ...rest].join(''))},
+    {"name": "Float", "symbols": ["Integer"], "postprocess": id},
+    {"name": "Float$ebnf$1", "symbols": [{"literal":"0"}], "postprocess": id},
+    {"name": "Float$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
+    {"name": "Float$ebnf$2", "symbols": [/[0-9]/]},
+    {"name": "Float$ebnf$2", "symbols": ["Float$ebnf$2", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Float", "symbols": ["Float$ebnf$1", {"literal":"."}, "Float$ebnf$2"], "postprocess": 
+        ([, , fractions]) => Number('.' + fractions.join(''))
+          },
+    {"name": "Float$ebnf$3", "symbols": []},
+    {"name": "Float$ebnf$3", "symbols": ["Float$ebnf$3", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Float$ebnf$4", "symbols": [/[0-9]/]},
+    {"name": "Float$ebnf$4", "symbols": ["Float$ebnf$4", /[0-9]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Float", "symbols": [/[1-9]/, "Float$ebnf$3", {"literal":"."}, "Float$ebnf$4"], "postprocess": 
+        ([first, rest, , fractions]) => Number([first, ...rest, '.', ...fractions].join(''))
+          }
 ]
   , ParserStart: "Main"
 }
