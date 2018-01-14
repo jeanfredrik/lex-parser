e46cc8239f4c2633d78781af9159f5a287e8821c
Fredrik Johansson <jeanfredrik@gmail.com>
2018-01-14 11:05:30 +0100

    🚚 Move index.js to src

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const grammar = require('./lex.js')
+const { Parser } = require('nearley')
+
+function parser() {
+  return new Parser(
+    grammar.ParserRules,
+    grammar.ParserStart,
+  )
+}
+
+function parse(input) {
+  const p = parser()
+  p.feed(input)
+  return p.results
+}
+
+module.exports = parser
+module.exports.parse = parse
