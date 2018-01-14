087f3591237c95b609e37e57ce7eb7ef8013820c
Fredrik Johansson <jeanfredrik@gmail.com>
2018-01-14 18:15:48 +0100

    📦 Build

--- dist/lex.js
+++ dist/lex.js
@@ -44,12 +44,12 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
     }, { "name": "Definition", "symbols": ["Identifier", "_", { "literal": "=" }, "DefList"], "postprocess": function postprocess(_ref5) {
         var _ref6 = _slicedToArray(_ref5, 4),
             identifier = _ref6[0],
-            items = _ref6[3];
+            symbols = _ref6[3];
 
         return {
           type: 'Definition',
           identifier: identifier,
-          items: items
+          symbols: symbols
         };
       }
     }, { "name": "DefListSep", "symbols": [{ "literal": "," }], "postprocess": function postprocess(d) {
@@ -65,28 +65,60 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
           return el[1];
         })));
       }
-    }, { "name": "DefListItem$subexpression$1", "symbols": ["_", "Grapheme", "_"] }, { "name": "DefListItem", "symbols": ["DefListItem$subexpression$1"], "postprocess": function postprocess(d) {
-        return d[0][1];
-      } }, { "name": "DefListItem", "symbols": ["_"], "postprocess": function postprocess(d) {
-        return '';
-      } }, { "name": "PatternWithWeight", "symbols": ["Pattern"], "postprocess": function postprocess(_ref9) {
-        var _ref10 = _slicedToArray(_ref9, 1),
-            pattern = _ref10[0];
+    }, { "name": "DefListItem", "symbols": ["_", "Grapheme", "_"], "postprocess": function postprocess(_ref9) {
+        var _ref10 = _slicedToArray(_ref9, 2),
+            grapheme = _ref10[1];
+
+        return {
+          type: 'DefinitionSymbol',
+          grapheme: grapheme
+        };
+      }
+    }, { "name": "DefListItem", "symbols": ["_", "Grapheme", "_", "Weight", "_"], "postprocess": function postprocess(_ref11) {
+        var _ref12 = _slicedToArray(_ref11, 4),
+            grapheme = _ref12[1],
+            weight = _ref12[3];
+
+        return {
+          type: 'DefinitionSymbol',
+          weight: weight,
+          grapheme: grapheme
+        };
+      }
+    }, { "name": "DefListItem", "symbols": ["_"], "postprocess": function postprocess() {
+        return {
+          type: 'DefinitionSymbol',
+          grapheme: ''
+        };
+      }
+    }, { "name": "DefListItem", "symbols": ["_", "Weight"], "postprocess": function postprocess(_ref13) {
+        var _ref14 = _slicedToArray(_ref13, 2),
+            weight = _ref14[1];
+
+        return {
+          type: 'DefinitionSymbol',
+          weight: weight,
+          grapheme: ''
+        };
+      }
+    }, { "name": "PatternWithWeight", "symbols": ["Pattern"], "postprocess": function postprocess(_ref15) {
+        var _ref16 = _slicedToArray(_ref15, 1),
+            pattern = _ref16[0];
 
         return Object.assign({ weight: 1 }, pattern);
       }
-    }, { "name": "PatternWithWeight", "symbols": ["Pattern", "_", "PatternWeight"], "postprocess": function postprocess(_ref11) {
-        var _ref12 = _slicedToArray(_ref11, 3),
-            pattern = _ref12[0],
-            weight = _ref12[2];
+    }, { "name": "PatternWithWeight", "symbols": ["Pattern", "_", "Weight"], "postprocess": function postprocess(_ref17) {
+        var _ref18 = _slicedToArray(_ref17, 3),
+            pattern = _ref18[0],
+            weight = _ref18[2];
 
         return Object.assign({}, pattern, {
           weight: weight
         });
       }
-    }, { "name": "PatternFragment", "symbols": ["Grapheme"], "postprocess": function postprocess(_ref13) {
-        var _ref14 = _slicedToArray(_ref13, 1),
-            grapheme = _ref14[0];
+    }, { "name": "PatternFragment", "symbols": ["Grapheme"], "postprocess": function postprocess(_ref19) {
+        var _ref20 = _slicedToArray(_ref19, 1),
+            grapheme = _ref20[0];
 
         return {
           type: 'PatternFragment',
@@ -99,17 +131,17 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         return null;
       } }, { "name": "Pattern$ebnf$1$subexpression$1", "symbols": ["Pattern$ebnf$1$subexpression$1$ebnf$1", "Pattern$ebnf$1$subexpression$1$ebnf$2"] }, { "name": "Pattern$ebnf$1", "symbols": ["Pattern$ebnf$1", "Pattern$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {
         return d[0].concat([d[1]]);
-      } }, { "name": "Pattern", "symbols": ["PatternFragment", "Pattern$ebnf$1"], "postprocess": function postprocess(_ref15) {
-        var _ref16 = _slicedToArray(_ref15, 2),
-            fragment = _ref16[0],
-            rest = _ref16[1];
+      } }, { "name": "Pattern", "symbols": ["PatternFragment", "Pattern$ebnf$1"], "postprocess": function postprocess(_ref21) {
+        var _ref22 = _slicedToArray(_ref21, 2),
+            fragment = _ref22[0],
+            rest = _ref22[1];
 
         return {
           type: 'Pattern',
-          parts: [fragment].concat(_toConsumableArray(rest.map(function (_ref17) {
-            var _ref18 = _slicedToArray(_ref17, 2),
-                subPatterns = _ref18[0],
-                fragment = _ref18[1];
+          parts: [fragment].concat(_toConsumableArray(rest.map(function (_ref23) {
+            var _ref24 = _slicedToArray(_ref23, 2),
+                subPatterns = _ref24[0],
+                fragment = _ref24[1];
 
             return [].concat(_toConsumableArray(subPatterns), _toConsumableArray(fragment == null ? [] : [fragment]));
           }).reduce(function (a, b) {
@@ -127,16 +159,16 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         return null;
       } }, { "name": "Pattern$ebnf$2$subexpression$2", "symbols": ["Pattern$ebnf$2$subexpression$2$ebnf$1", "Pattern$ebnf$2$subexpression$2$ebnf$2"] }, { "name": "Pattern$ebnf$2", "symbols": ["Pattern$ebnf$2", "Pattern$ebnf$2$subexpression$2"], "postprocess": function arrpush(d) {
         return d[0].concat([d[1]]);
-      } }, { "name": "Pattern", "symbols": ["Pattern$ebnf$2"], "postprocess": function postprocess(_ref19) {
-        var _ref20 = _slicedToArray(_ref19, 1),
-            rest = _ref20[0];
+      } }, { "name": "Pattern", "symbols": ["Pattern$ebnf$2"], "postprocess": function postprocess(_ref25) {
+        var _ref26 = _slicedToArray(_ref25, 1),
+            rest = _ref26[0];
 
         return {
           type: 'Pattern',
-          parts: rest.map(function (_ref21) {
-            var _ref22 = _slicedToArray(_ref21, 2),
-                subPatterns = _ref22[0],
-                fragment = _ref22[1];
+          parts: rest.map(function (_ref27) {
+            var _ref28 = _slicedToArray(_ref27, 2),
+                subPatterns = _ref28[0],
+                fragment = _ref28[1];
 
             return [].concat(_toConsumableArray(subPatterns), _toConsumableArray(fragment == null ? [] : [fragment]));
           }).reduce(function (a, b) {
@@ -144,39 +176,39 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
           }, [])
         };
       }
-    }, { "name": "SubPattern", "symbols": [{ "literal": "(" }, "Pattern", { "literal": ")" }], "postprocess": function postprocess(_ref23) {
-        var _ref24 = _slicedToArray(_ref23, 2),
-            pattern = _ref24[1];
+    }, { "name": "SubPattern", "symbols": [{ "literal": "(" }, "Pattern", { "literal": ")" }], "postprocess": function postprocess(_ref29) {
+        var _ref30 = _slicedToArray(_ref29, 2),
+            pattern = _ref30[1];
 
         return Object.assign({ weight: .5 }, pattern);
       }
-    }, { "name": "SubPattern", "symbols": [{ "literal": "(" }, "Pattern", "_", "PatternWeight", { "literal": ")" }], "postprocess": function postprocess(_ref25) {
-        var _ref26 = _slicedToArray(_ref25, 4),
-            pattern = _ref26[1],
-            weight = _ref26[3];
+    }, { "name": "SubPattern", "symbols": [{ "literal": "(" }, "Pattern", "_", "Weight", { "literal": ")" }], "postprocess": function postprocess(_ref31) {
+        var _ref32 = _slicedToArray(_ref31, 4),
+            pattern = _ref32[1],
+            weight = _ref32[3];
 
         return Object.assign({}, pattern, {
           weight: weight
         });
       }
-    }, { "name": "PatternWeight", "symbols": [{ "literal": "*" }, "_", "Float"], "postprocess": function postprocess(_ref27) {
-        var _ref28 = _slicedToArray(_ref27, 3),
-            value = _ref28[2];
+    }, { "name": "Weight", "symbols": [{ "literal": "*" }, "_", "Float"], "postprocess": function postprocess(_ref33) {
+        var _ref34 = _slicedToArray(_ref33, 3),
+            value = _ref34[2];
 
         return value;
       }
-    }, { "name": "PatternWeight", "symbols": [{ "literal": "/" }, "_", "Float"], "postprocess": function postprocess(_ref29) {
-        var _ref30 = _slicedToArray(_ref29, 3),
-            value = _ref30[2];
+    }, { "name": "Weight", "symbols": [{ "literal": "/" }, "_", "Float"], "postprocess": function postprocess(_ref35) {
+        var _ref36 = _slicedToArray(_ref35, 3),
+            value = _ref36[2];
 
         return 1 / value;
       }
     }, { "name": "Identifier$subexpression$1", "symbols": ["Alpha"] }, { "name": "Identifier$subexpression$1", "symbols": ["Unicode"] }, { "name": "Identifier$ebnf$1", "symbols": [] }, { "name": "Identifier$ebnf$1$subexpression$1", "symbols": ["Alpha"] }, { "name": "Identifier$ebnf$1$subexpression$1", "symbols": ["Unicode"] }, { "name": "Identifier$ebnf$1$subexpression$1", "symbols": ["Digit"] }, { "name": "Identifier$ebnf$1", "symbols": ["Identifier$ebnf$1", "Identifier$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {
         return d[0].concat([d[1]]);
-      } }, { "name": "Identifier", "symbols": ["Identifier$subexpression$1", "Identifier$ebnf$1"], "postprocess": function postprocess(_ref31) {
-        var _ref32 = _slicedToArray(_ref31, 2),
-            first = _ref32[0],
-            rest = _ref32[1];
+      } }, { "name": "Identifier", "symbols": ["Identifier$subexpression$1", "Identifier$ebnf$1"], "postprocess": function postprocess(_ref37) {
+        var _ref38 = _slicedToArray(_ref37, 2),
+            first = _ref38[0],
+            rest = _ref38[1];
 
         return "" + first + rest.map(id).join('');
       }
@@ -201,19 +233,19 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         return 0;
       } }, { "name": "Integer$ebnf$1", "symbols": [] }, { "name": "Integer$ebnf$1", "symbols": ["Integer$ebnf$1", /[0-9]/], "postprocess": function arrpush(d) {
         return d[0].concat([d[1]]);
-      } }, { "name": "Integer", "symbols": [/[1-9]/, "Integer$ebnf$1"], "postprocess": function postprocess(_ref33) {
-        var _ref34 = _slicedToArray(_ref33, 2),
-            first = _ref34[0],
-            rest = _ref34[1];
+      } }, { "name": "Integer", "symbols": [/[1-9]/, "Integer$ebnf$1"], "postprocess": function postprocess(_ref39) {
+        var _ref40 = _slicedToArray(_ref39, 2),
+            first = _ref40[0],
+            rest = _ref40[1];
 
         return Number([first].concat(_toConsumableArray(rest)).join(''));
       } }, { "name": "Float", "symbols": ["Integer"], "postprocess": id }, { "name": "Float$ebnf$1", "symbols": [{ "literal": "0" }], "postprocess": id }, { "name": "Float$ebnf$1", "symbols": [], "postprocess": function postprocess(d) {
         return null;
       } }, { "name": "Float$ebnf$2", "symbols": [/[0-9]/] }, { "name": "Float$ebnf$2", "symbols": ["Float$ebnf$2", /[0-9]/], "postprocess": function arrpush(d) {
         return d[0].concat([d[1]]);
-      } }, { "name": "Float", "symbols": ["Float$ebnf$1", { "literal": "." }, "Float$ebnf$2"], "postprocess": function postprocess(_ref35) {
-        var _ref36 = _slicedToArray(_ref35, 3),
-            fractions = _ref36[2];
+      } }, { "name": "Float", "symbols": ["Float$ebnf$1", { "literal": "." }, "Float$ebnf$2"], "postprocess": function postprocess(_ref41) {
+        var _ref42 = _slicedToArray(_ref41, 3),
+            fractions = _ref42[2];
 
         return Number('.' + fractions.join(''));
       }
@@ -221,11 +253,11 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         return d[0].concat([d[1]]);
       } }, { "name": "Float$ebnf$4", "symbols": [/[0-9]/] }, { "name": "Float$ebnf$4", "symbols": ["Float$ebnf$4", /[0-9]/], "postprocess": function arrpush(d) {
         return d[0].concat([d[1]]);
-      } }, { "name": "Float", "symbols": [/[1-9]/, "Float$ebnf$3", { "literal": "." }, "Float$ebnf$4"], "postprocess": function postprocess(_ref37) {
-        var _ref38 = _slicedToArray(_ref37, 4),
-            first = _ref38[0],
-            rest = _ref38[1],
-            fractions = _ref38[3];
+      } }, { "name": "Float", "symbols": [/[1-9]/, "Float$ebnf$3", { "literal": "." }, "Float$ebnf$4"], "postprocess": function postprocess(_ref43) {
+        var _ref44 = _slicedToArray(_ref43, 4),
+            first = _ref44[0],
+            rest = _ref44[1],
+            fractions = _ref44[3];
 
         return Number([first].concat(_toConsumableArray(rest), ['.'], _toConsumableArray(fractions)).join(''));
       }
