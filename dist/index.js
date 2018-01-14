28beeace5b8921505ed4631a14bef5399dcb5999
Fredrik Johansson <jeanfredrik@gmail.com>
2018-01-14 11:06:37 +0100

    📦 Build

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,19 @@
-const grammar = require('./lex.js')
-const { Parser } = require('nearley')
+'use strict';
+
+var grammar = require('./lex.js');
+
+var _require = require('nearley'),
+    Parser = _require.Parser;
 
 function parser() {
-  return new Parser(
-    grammar.ParserRules,
-    grammar.ParserStart,
-  )
+  return new Parser(grammar.ParserRules, grammar.ParserStart);
 }
 
 function parse(input) {
-  const p = parser()
-  p.feed(input)
-  return p.results
+  var p = parser();
+  p.feed(input);
+  return p.results;
 }
 
-module.exports = parser
-module.exports.parse = parse
+module.exports = parser;
+module.exports.parse = parse;
